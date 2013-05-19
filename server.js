108c7e8b7a84706775afc7c09630a6094f8d9192
Jon Solove <jon.solove@gmail.com>
2013-05-18 20:44:49 -0600

    Changing to port 3000

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ function start(route) {
     response.end();
   }
 
-  http.createServer(onRequest).listen(8888);
+  http.createServer(onRequest).listen(3000);
   console.log("Server has started.");
 }
 
