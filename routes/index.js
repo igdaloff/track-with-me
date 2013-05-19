b1eefabf60aa7eabd5d593e2ecc7ba030c54ca24
Jon Solove <jon.solove@gmail.com>
2013-05-19 17:59:03 -0600

    more cleanup - removing header/footer holder text (annoying), displaying username in home screen if logged in.

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,10 @@
 
  exports.index = function(req, res){
 
+ 	var displayName = '';
  	if (req.isAuthenticated() == true) {
- 		console.log("user: " + req.user);
+ 		displayName = req.user.displayName;
  	}
- 	
- 	res.render('index', { title: 'Track With Me.', isAuthenticated: req.isAuthenticated() });
+
+ 	res.render('index', { title: 'Track With Me.', isAuthenticated: req.isAuthenticated(), displayName: displayName });
  };
