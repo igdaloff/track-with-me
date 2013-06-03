6bbb9f920bc2caffc9cf21d89e0c74e2d19d4691
Jon Solove <jon.solove@gmail.com>
2013-06-02 21:09:02 -0600

    Using unveil (sorta) to lazy load images when the modal is opened.

--- public/javascripts/date.js
+++ public/javascripts/date.js
@@ -5,5 +5,18 @@ $(document).ready( function() {
 	$('#date-progress').animate({
 	  width: '100%'
 	}, mixDuration, 'linear');
+
+	$('img.facebook-image').unveil();
+
+	/* 	Sorta bastardizing Unveil (http://luis-almeida.github.io/unveil/), 
+		but this will work for now.  Doesn't load image until you go into 
+		the add-friends modal.  This is ugly, i'd like to get regular unveil 
+		to work...                                                   			*/
+
+	$('a.add-friend').click(function(){
+		$('img.facebook-image').trigger("unveil");
+	})
+
+	
 });
 
